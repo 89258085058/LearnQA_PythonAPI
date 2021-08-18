619dfba00f81ddb90f36cd2f1e2dafa78136ea24
Alexandr Gorelov <gorelov2895@yandex.ru>
2021-08-18 11:17:08 +0300

    Ex6: Длинный редирект

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
-print(response.text)
+response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
+count = len(response.history)
+last_redirect = response.history[-1]
+first_redirect = response.history[0]
+second_redirect = response.history[1]
+third_redirect = response.history[2]
+#Выводим сколько редиректов происходит от изначальной точки назначения до итоговой
+print('Count_redirect= ', count - 1)
+#Выводим какие ULR редиктов до итоговой
+print('URL_redirect= ', last_redirect.url, second_redirect.url, third_redirect.url)
+#Выврдим итоговый url
+print ('last_redirect= ', last_redirect.url)
