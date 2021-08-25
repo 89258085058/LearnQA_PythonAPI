2d99ec40ae69f2bffe0ddc02677e83c29b76eed2
89258085058 <86528556+89258085058@users.noreply.github.com>
2021-08-25 18:50:45 +0300

    Ex12: Тест запроса на метод header

--- test_ex12.py
+++ test_ex12.py
@@ -1,24 +1,11 @@
 import requests
 
+header = {'x-secret-homework-header': 'Some secret value'}
 
 url = "https://playground.learnqa.ru/api/homework_header"
-response = requests.get(url)
-print(response.headers)
-headers = response.headers
-correct_headers = {
-  'Date': 'Wed, 25 Aug 2021 14:00:55 GMT',
-  'Content-Type': 'application/json',
-  'Content-Length': '15',
-  'Connection': 'keep-alive',
-  'Keep-Alive': 'timeout=10',
-  'Server': 'Apache',
-  'x-secret-homework-header': 'Some secret value',
-  'Cache-Control': 'max-age=0',
-  'Expires': 'Wed, 25 Aug 2021 14:00:55 GMT'
-}
+response = requests.get(url, params=header)
+response_header = response.text
+response1 = '{"success":"!"}'
 
+assert response_header == response1, f"HEADER: {response_header} does not match the expected one"
 
-url = "https://playground.learnqa.ru/api/homework_header"
-headerss = {'x-secret-homework-header': 'Some secret value'}
-response1 = requests.head(url, params=headerss)
-print(response1.text)
