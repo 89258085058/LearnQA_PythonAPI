ec271ee8bab3834123ef3fe7b50716cac2645adf
89258085058 <86528556+89258085058@users.noreply.github.com>
2021-09-01 20:57:27 +0300

    Просмотр пользователя

--- lib/assertions.py
+++ lib/assertions.py
@@ -8,7 +8,41 @@ class Assertions:
         try:
             responce_as_dict = responce.json()
         except json.JSONDecodeError:
-            assert False, f"Responce is not in JSON format. Responce text is '{responce.text}"
+            assert False, f"Responce is not in JSON format. Responce text is '{responce.text}'"
 
-        assert name in responce_as_dict, f"Responce JSON doesn't have key '{name}"
+        assert name in responce_as_dict, f"Responce JSON doesn't have key '{name}'"
         assert responce_as_dict[name] == expected_value, error_message
+
+    @staticmethod
+    def assert_json_has_key(response: Response, name):
+        try:
+            responce_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Responce is not in JSON format. Responce text is '{response.text}'"
+
+        assert name in responce_as_dict, f"Responce JSON doesn't have key '{name}'"
+
+    @staticmethod
+    def assert_code_status(responce: Response, expected_status_code):
+        assert responce.status_code == expected_status_code, \
+            f"Unexpected status code! Expected: {expected_status_code}. Actual: {responce.status_code}"
+
+    @staticmethod
+    def assert_json_has_not_key(responce: Response, name):
+        try:
+            responce_as_dict = responce.json()
+        except json.JSONDecodeError:
+            assert False, f"Responce is not in JSON format. Responce text is '{responce.text}'"
+
+        assert name not in responce_as_dict, f"Responce JSON shouldn't have key '{name}'. But it's present"
+
+    @staticmethod
+    def assert_json_has_keys(response: Response, names: list):
+        try:
+            responce_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Responce is not in JSON format. Responce text is '{response.text}'"
+        for name in names:
+            assert name in responce_as_dict, f"Responce JSON doesn't have key '{name}'"
+
+
