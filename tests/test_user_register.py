6d73b5af4ec885c4f132b707cfb7bd7595107fe5
89258085058 <86528556+89258085058@users.noreply.github.com>
2021-09-05 18:32:42 +0300

    Ex15: Тесты на метод user

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -2,6 +2,15 @@ from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 import allure
+import pytest
+
+params = [
+    ("username"),
+    ("firstName"),
+    ("lastName"),
+    ("email"),
+    ("password")
+]
 
 @allure.epic("Authorization cases")
 class TestUserRegister(BaseCase):
@@ -25,3 +34,48 @@ class TestUserRegister(BaseCase):
 
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
+
+
+    @allure.description("This test doesn't register user with email without '@'")
+    def test_create_user_with_incorrect_email(self):
+        email = 'vinkotov-example.com'
+        data = self.prepare_registration_data(email)
+        response = MyRequests.post("/user/", data=data)
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == f"Invalid email format", \
+            f"Unexpected response content {response.content}"
+
+
+    @allure.description("This test doesn't register user missed param")
+    @pytest.mark.parametrize('param', params)
+    def test_create_user_without_param(self, param):
+        data = self.prepare_registration_data()
+        data.pop(param)
+        missed_param = data
+        response = MyRequests.post("/user/", data=missed_param)
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == f"The following required params are missed: {param}", \
+            f"Unexpected response content {response.content} with missed param: {param}"
+
+
+    @allure.description("This test doesn't register user with short name")
+    def test_creating_user_with_short_name(self):
+        data = self.prepare_registration_data()
+        data['firstName'] = 'L'
+        response = MyRequests.post("/user/", data=data)
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == f"The value of 'firstName' field is too short", \
+            f"Unexpected response content for field 'firstName' when it is too short"
+
+
+    @allure.description("This test doesn't register user with a very long name-longer than 250 characters")
+    def test_create_user_long_name(self):
+        data = self.prepare_registration_data()
+        long_name = ""
+        while len(long_name) <= 250:
+            long_name += "l"
+        data['firstName'] = long_name
+        response = MyRequests.post("/user/", data=data)
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == f"The value of 'firstName' field is too long", \
+            f"Unexpected response content for field 'firstName' when it is too long"
